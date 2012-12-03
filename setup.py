9bb3dedd7846701616f2743fa7a0bbfee618de65
Rafal Stozek <say4ne@gmail.com>
2012-12-03 16:05:40 +0100

    Added python 3 classifiers

--- setup.py
+++ setup.py
@@ -46,6 +46,8 @@ setup(
         'Programming Language :: Python :: 2.5',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
         'Topic :: Utilities',
     ],
 )
