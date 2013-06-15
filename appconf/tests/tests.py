de23f5213913b3d6ea3e008bc01b48770dc70554
Jannis Leidel <jannis@leidel.info>
2013-06-15 15:16:09 +0200

    Use correct test methods.

--- appconf/tests/tests.py
+++ appconf/tests/tests.py
@@ -11,42 +11,42 @@ from appconf.tests.models import (AppConf, TestConf, PrefixConf,
 class TestConfTests(TestCase):
 
     def test_basic(self):
-        self.assertEquals(TestConf._meta.prefix, 'tests')
+        self.assertEqual(TestConf._meta.prefix, 'tests')
 
     def test_simple(self):
         self.assertTrue(hasattr(settings, 'TESTS_SIMPLE_VALUE'))
-        self.assertEquals(settings.TESTS_SIMPLE_VALUE, True)
+        self.assertEqual(settings.TESTS_SIMPLE_VALUE, True)
 
     def test_configured(self):
         self.assertTrue(hasattr(settings, 'TESTS_CONFIGURED_VALUE'))
-        self.assertEquals(settings.TESTS_CONFIGURED_VALUE, 'correct')
+        self.assertEqual(settings.TESTS_CONFIGURED_VALUE, 'correct')
 
     def test_configure_method(self):
         self.assertTrue(hasattr(settings, 'TESTS_CONFIGURE_METHOD_VALUE'))
-        self.assertEquals(settings.TESTS_CONFIGURE_METHOD_VALUE, True)
+        self.assertEqual(settings.TESTS_CONFIGURE_METHOD_VALUE, True)
 
     def test_init_kwargs(self):
         custom_conf = TestConf(CUSTOM_VALUE='custom')
-        self.assertEquals(custom_conf.CUSTOM_VALUE, 'custom')
-        self.assertEquals(settings.TESTS_CUSTOM_VALUE, 'custom')
+        self.assertEqual(custom_conf.CUSTOM_VALUE, 'custom')
+        self.assertEqual(settings.TESTS_CUSTOM_VALUE, 'custom')
         self.assertRaises(AttributeError,
                           lambda: custom_conf.TESTS_CUSTOM_VALUE)
         custom_conf.CUSTOM_VALUE_SETATTR = 'custom'
-        self.assertEquals(settings.TESTS_CUSTOM_VALUE_SETATTR, 'custom')
+        self.assertEqual(settings.TESTS_CUSTOM_VALUE_SETATTR, 'custom')
         custom_conf.custom_value_lowercase = 'custom'
         self.assertRaises(AttributeError,
                           lambda: settings.custom_value_lowercase)
 
     def test_init_kwargs_with_prefix(self):
         custom_conf = TestConf(TESTS_CUSTOM_VALUE2='custom2')
-        self.assertEquals(custom_conf.TESTS_CUSTOM_VALUE2, 'custom2')
-        self.assertEquals(settings.TESTS_CUSTOM_VALUE2, 'custom2')
+        self.assertEqual(custom_conf.TESTS_CUSTOM_VALUE2, 'custom2')
+        self.assertEqual(settings.TESTS_CUSTOM_VALUE2, 'custom2')
 
     def test_proxy(self):
         custom_conf = ProxyConf(CUSTOM_VALUE3='custom3')
-        self.assertEquals(custom_conf.CUSTOM_VALUE3, 'custom3')
-        self.assertEquals(settings.TESTS_CUSTOM_VALUE3, 'custom3')
-        self.assertEquals(custom_conf.TESTS_CUSTOM_VALUE3, 'custom3')
+        self.assertEqual(custom_conf.CUSTOM_VALUE3, 'custom3')
+        self.assertEqual(settings.TESTS_CUSTOM_VALUE3, 'custom3')
+        self.assertEqual(custom_conf.TESTS_CUSTOM_VALUE3, 'custom3')
         self.assertTrue('appconf.tests' in custom_conf.INSTALLED_APPS)
 
     def test_dir_members(self):
@@ -62,63 +62,63 @@ class TestConfTests(TestCase):
     def test_custom_holder(self):
         CustomHolderConf()
         self.assertTrue(hasattr(custom_holder, 'CUSTOM_HOLDER_SIMPLE_VALUE'))
-        self.assertEquals(custom_holder.CUSTOM_HOLDER_SIMPLE_VALUE, True)
+        self.assertEqual(custom_holder.CUSTOM_HOLDER_SIMPLE_VALUE, True)
 
     def test_subclass_configured_data(self):
         self.assertTrue('TESTS_CONFIGURE_METHOD_VALUE2' in dir(settings))
-        self.assertEquals(settings.TESTS_CONFIGURE_METHOD_VALUE2, False)
+        self.assertEqual(settings.TESTS_CONFIGURE_METHOD_VALUE2, False)
 
 
 class PrefixConfTests(TestCase):
 
     def test_prefix(self):
-        self.assertEquals(PrefixConf._meta.prefix, 'prefix')
+        self.assertEqual(PrefixConf._meta.prefix, 'prefix')
 
     def test_simple(self):
         self.assertTrue(hasattr(settings, 'PREFIX_SIMPLE_VALUE'))
-        self.assertEquals(settings.PREFIX_SIMPLE_VALUE, True)
+        self.assertEqual(settings.PREFIX_SIMPLE_VALUE, True)
 
     def test_configured(self):
         self.assertTrue(hasattr(settings, 'PREFIX_CONFIGURED_VALUE'))
-        self.assertEquals(settings.PREFIX_CONFIGURED_VALUE, 'correct')
+        self.assertEqual(settings.PREFIX_CONFIGURED_VALUE, 'correct')
 
     def test_configure_method(self):
         self.assertTrue(hasattr(settings, 'PREFIX_CONFIGURE_METHOD_VALUE'))
-        self.assertEquals(settings.PREFIX_CONFIGURE_METHOD_VALUE, True)
+        self.assertEqual(settings.PREFIX_CONFIGURE_METHOD_VALUE, True)
 
 
 class YetAnotherPrefixConfTests(TestCase):
 
     def test_prefix(self):
-        self.assertEquals(YetAnotherPrefixConf._meta.prefix,
-                          'yetanother_prefix')
+        self.assertEqual(YetAnotherPrefixConf._meta.prefix,
+                         'yetanother_prefix')
 
     def test_simple(self):
         self.assertTrue(hasattr(settings,
                                 'YETANOTHER_PREFIX_SIMPLE_VALUE'))
-        self.assertEquals(settings.YETANOTHER_PREFIX_SIMPLE_VALUE, False)
+        self.assertEqual(settings.YETANOTHER_PREFIX_SIMPLE_VALUE, False)
 
     def test_configured(self):
         self.assertTrue(hasattr(settings,
                                 'YETANOTHER_PREFIX_CONFIGURED_VALUE'))
-        self.assertEquals(settings.YETANOTHER_PREFIX_CONFIGURED_VALUE,
-                          'correct')
+        self.assertEqual(settings.YETANOTHER_PREFIX_CONFIGURED_VALUE,
+                         'correct')
 
     def test_configure_method(self):
         self.assertTrue(hasattr(settings,
                                 'YETANOTHER_PREFIX_CONFIGURE_METHOD_VALUE'))
-        self.assertEquals(settings.YETANOTHER_PREFIX_CONFIGURE_METHOD_VALUE,
-                          True)
+        self.assertEqual(settings.YETANOTHER_PREFIX_CONFIGURE_METHOD_VALUE,
+                         True)
 
 
 class SeparateConfTests(TestCase):
 
     def test_prefix(self):
-        self.assertEquals(SeparateConf._meta.prefix, 'prefix')
+        self.assertEqual(SeparateConf._meta.prefix, 'prefix')
 
     def test_simple(self):
         self.assertTrue(hasattr(settings, 'PREFIX_SEPARATE_VALUE'))
-        self.assertEquals(settings.PREFIX_SEPARATE_VALUE, True)
+        self.assertEqual(settings.PREFIX_SEPARATE_VALUE, True)
 
 
 class RequiredSettingsTests(TestCase):
